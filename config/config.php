39ec63bd47295e8184969fdd9e33efd3753aaebf
wwWatcher <nrbox2000-register@yahoo.de>
2018-04-21 09:53:38 +0200

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,40 @@
+<?php
+/*************************************************************
+* Required parameters for system
+* if the system is located at different site, these parameter 
+* should be changed for each site's identity
+**************************************************************/
+
+/**
+* Site name
+*/
+$cfg['site']['name'] = "Start your own MVC Framework with PHP";
+/**
+* Site owner name
+*/
+$cfg['site']['owner'] = "Elvis is still in the building";
+/**
+* Page title
+*/
+$cfg['site']['title'] = $cfg['site']['name'] . " : " . $cfg['site']['name'];
+/**
+* site address
+*/
+$cfg['site']['address'] = 'http://www.elvishsu66.com';
+/**
+* company logo
+*/
+$cfg['site']['logo'] = 'logo.png';
+/**
+* The software version 
+*/
+$cfg['site']['version'] = '0.1';
+
+
+/************************************************************* 
+* Database related settings
+*************************************************************/
+
+
+
+?>
